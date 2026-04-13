cc910a5bc91aea389435b8933f881e2f8d0c9b6c
jozelot_ <jozelot77@gmail.com>
2026-04-13 21:53:20 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,42 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Jozelot Docs',
+			logo: {
+				src: './src/assets/favicon.png',
+				replacesTitle: false,
+			},
+			favicon: '/favicon.png',
+			customCss: ['./src/styles/custom.css'],
+			components: {
+				Footer: './src/components/CustomFooter.astro',
+			},
+			// credits: true,
+			head: [
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.googleapis.com',
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'preconnect',
+						href: 'https://fonts.gstatic.com',
+						crossorigin: true,
+					},
+				},
+				{
+					tag: 'link',
+					attrs: {
+						rel: 'stylesheet',
+						href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap',
+					},
+				},
+			],
+			social: [{ icon: 'external', label: 'Website', href: 'https://jozelot.de' }, { icon: 'github', label: 'GitHub', href: 'https://github.com/derjozelot' }, { icon: 'seti:json', label: 'code', href: 'https://jd.jozelot.de' }, { icon: 'discord', label: 'Discord', href: 'https://jozelot.de/discord' }],
 			sidebar: [
 				{
 					label: 'Guides',
